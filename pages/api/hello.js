87939604ebce8abd5695bca319c43bf478ef66e4
Tim Veletta <tim.veletta@mechanicalrock.io>
2023-02-03 13:44:57 +0800

    update api endpoint to ping edge config

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getAll } from '@vercel/edge-config';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+	if (req.method === 'GET') {
+		const { greeting } = await getAll(['greeting']);
+		res.status(200).json({ greeting });
+	} else if (req.method === 'POST') {
+		const greeting = req.body.greeting;
+		const result = await fetch(
+			'https://api.vercel.com/v1/edge-config/ecfg_ntchbrxqkjbwhtorydhhkmoxkxto/items',
+			{
+				method: 'PATCH',
+				headers: {
+					Authorization: `Bearer ${process.env.EDGE_CONFIG}`,
+					'content-type': 'application/json',
+				},
+				body: JSON.stringify({
+					items: [
+						{
+							operation: 'update',
+							key: 'greeting',
+							value: greeting,
+						},
+					],
+				}),
+			}
+		);
+		const json = await result.json();
+		if (json.status !== 'ok') {
+			console.error('Failed to update edge config', json);
+		}
+		res.status(200).json({ greeting });
+	}
 }
